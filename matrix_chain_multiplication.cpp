feb8d5616ebb5c009deb425857236b0a6df24bad
nujellaharshini <122059803+nujellaharshini@users.noreply.github.com>
2025-01-22 22:42:21 -0800

    Update matrix_chain_multiplication.cpp

--- matrix_chain_multiplication.cpp
+++ matrix_chain_multiplication.cpp
@@ -29,6 +29,22 @@ int main() {
     delete [] p;
     return 0;
 }
+/** matrix-chain-order pseudo: 
+    n = p.length - 1
+    let m[1...n, 1...n] and s[1...n-1, 2...n] be new tables
+    for i = 1 to n
+        m[i,j] = 0
+    for l = 2 to n
+        for i = 1 to n - l + 1
+            j = i + l - 1
+            m[i,j] = inf
+            for k = i to j - 1
+                q = m[i,k] + m[k + 1,j] + p(i-1)*p(k)*p(j)
+                if q < m[i,j]
+                    m[i,j] = q
+                    s[i,j] = k
+    return m and s
+**/
 
 pair<vector<vector<int>>, vector<vector<int>>> MatrixChainOrder(int *p) {
 // compute m[i][j] and s[i][j] using DP
